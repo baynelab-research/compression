d9bb0b6b8fbd6cb83a009251c8fad9d3942f022b
ecknight <ellyknight@gmail.com>
2022-12-09 16:46:30 -0700

    Remove score, do figures

--- recognizer_analysis.R
+++ recognizer_analysis.R
@@ -1,8 +1,11 @@
 library(tidyverse)
 library(lme4)
 library(MuMIn)
+library(gridExtra)
 
-#NOTE: 100 recordings with confirmed detections were scanned for each species and for each treatment combination----
+#NOTE: 100 recordings with confirmed detections were scanned for each species and for each treatment combination
+
+#TO DO: NEED TO INVESTIGATE NUMBER OF FILES RUN FOR 96 X 44100
 
 #1. Read in data----
 oven <- read.csv("data/recognizer/CompiledCNNResults_OVEN_Validated.csv") %>%
@@ -18,13 +21,13 @@ hit <- rbind(oven, coni) %>%
                                 samplerate==32000 ~ 22050,
                                 samplerate==44100 ~ 44100),
          compressiontype = ifelse(filetype=="wav", "wav", paste0("mp3-", compressionrate)),
-         compressiontype = factor(compressiontype, levels=c("wav", "mp3-96", "mp3-320")),
-         samplerate.s = as.numeric(scale(samplerate))) %>% 
+         compressiontype = factor(compressiontype, levels=c("wav", "mp3-320", "mp3-96")),
+         samplerate.s = (samplerate-22050)/22050) %>% 
   dplyr::filter(!is.na(detection))
 
 #3. Summarize at recording level----
 rec <- hit %>% 
-  group_by(recording, species, samplerate.s, compressiontype) %>% 
+  group_by(recording, species, samplerate, samplerate.s, compressiontype) %>% 
   summarize(n=n(),
             hits = sum(detection)) %>% 
   ungroup() %>% 
@@ -56,12 +59,7 @@ ggplot(rec) +
   geom_smooth(aes(x=samplerate.s, y=precision, colour=species)) +
   facet_grid(species~compressiontype, scales="free")
 
-#6b. Score----
-ggplot(hit) +
-  geom_boxplot(aes(x=factor(samplerate.s), y=score, colour=species)) +
-  facet_grid(species~compressiontype)
-
-#6c. Recall----
+#6b. Recall----
 ggplot(recall) +
   geom_smooth(aes(x=samplerate.s, y=hit, colour=species)) +
   facet_grid(species~ compressiontype)
@@ -70,49 +68,27 @@ ggplot(recall) +
 
 #7a. Probability of detection being true----
 #CONI
-hit.coni <- dplyr::filter(hit, species=="CONI")
-h0.coni <- glmer(detection ~ samplerate.s*compressiontype + (1|recording),
-            data=hit.coni, na.action = "na.fail", family="binomial")
+rec.coni <- dplyr::filter(rec, species=="CONI")
+h0.coni <- glm(precision ~ samplerate.s*compressiontype,
+            data=rec.coni, na.action = "na.fail")
 dredge(h0.coni)
 
-h1.coni <- glmer(detection ~ samplerate.s + (1|recording),
-            data=hit.coni, na.action = "na.fail", family="binomial")
+h1.coni <- lm(precision ~ 1, data=rec.coni)
 summary(h1.coni)
-#Small effect of sample rate
+#no effect
 
 #OVEN
-hit.oven <- dplyr::filter(hit, species=="OVEN")
-h0.oven <- glmer(detection ~ samplerate.s*compressiontype + (1|recording),
-                 data=hit.oven, na.action = "na.fail", family="binomial")
+rec.oven <- dplyr::filter(rec, species=="OVEN")
+h0.oven <- lm(precision ~ samplerate.s*compressiontype,
+                 data=rec.oven, na.action = "na.fail")
 dredge(h0.oven)
 
-h1.oven <- glmer(detection ~ samplerate.s + (1|recording),
-                 data=hit.oven, na.action = "na.fail", family="binomial")
+h1.oven <- lm(precision ~ samplerate.s,
+              data=rec.oven, na.action = "na.fail")
 summary(h1.oven)
 #Strong effect of sample rate
 
-#7b. Score value----
-#CONI
-s0.coni <- glmer(score ~ samplerate.s*compressiontype + (1|recording),
-            data=hit.coni, na.action = "na.fail", family="Gamma")
-dredge(s0.coni)
-
-s1.coni <- glmer(score ~ compressiontype + (1|recording),
-           data=hit.coni, na.action = "na.fail", family="Gamma")
-summary(s1.coni)
-plot(s1.coni)
-
-#OVEN
-s0.oven <- glmer(score ~ samplerate.s*compressiontype + (1|recording),
-                 data=hit.oven, na.action = "na.fail", family="Gamma")
-dredge(s0.oven)
-
-s1.oven <- glmer(score ~ compressiontype + (1|recording),
-                 data=hit.oven, na.action = "na.fail", family="Gamma")
-summary(s1.oven)
-plot(s1.oven)
-
-#7c. Recording level recall----
+#7b. Recording level recall----
 #CONI
 recall.coni <- dplyr::filter(recall, species=="CONI")
 r0.coni <- glm(hit ~ samplerate.s*compressiontype, data=recall.coni, na.action="na.fail")
@@ -125,7 +101,86 @@ summary(r1.coni)
 recall.oven <- dplyr::filter(recall, species=="OVEN")
 r0.oven <- glm(hit ~ samplerate.s*compressiontype, data=recall.oven, na.action="na.fail")
 dredge(r0.oven)
-summary(r0.oven)
+
+r1.oven <- r0.oven
+summary(r1.oven)
 
 #8. Plot----
-#Sample rate on p(true positive)
+my.theme <- theme_classic() +
+  theme(text=element_text(size=12, family="Arial"),
+        axis.text.x=element_text(size=12),
+        axis.text.y=element_text(size=12),
+        axis.title.x=element_text(margin=margin(10,0,0,0)),
+        axis.title.y=element_text(margin=margin(0,10,0,0)),
+        axis.line.x=element_line(linetype=1),
+        axis.line.y=element_line(linetype=1),
+        legend.text=element_text(size=12),
+        legend.title=element_text(size=12),
+        plot.title=element_text(size=12, hjust = 0.5))
+
+newdat <- expand.grid(compressiontype=unique(hit$compressiontype),
+                      samplerate.s = seq(0, 1, 0.01))
+
+#8a. Effect of sample rate on p(true positive)----
+h.pred <- data.frame(pred = predict(h1.oven, newdat, se=TRUE)) %>% 
+  cbind(newdat) %>% 
+  mutate(species="OVEN") %>% 
+  rbind(data.frame(pred = predict(h1.coni, newdat, se=TRUE)) %>% 
+          cbind(newdat) %>% 
+          mutate(species="CONI")) %>% 
+  dplyr::select(pred.fit, pred.se.fit, samplerate.s, species) %>% 
+  unique() %>% 
+  mutate(samplerate = samplerate.s*22050 + 22050,
+         upper = pred.fit + 1.96*pred.se.fit,
+         lower = pred.fit - 1.96*pred.se.fit)
+
+h.pred$species <- factor(h.pred$species, labels=c("Common Nighthawk", "Ovenbird"))
+rec$species <- factor(rec$species, labels=c("Common Nighthawk", "Ovenbird"))
+
+h.plot <- ggplot() +
+  geom_point(data=rec, aes(x=samplerate, y=precision), pch=21, colour="grey50", fill="grey70") +
+  geom_ribbon(data=h.pred, aes(x=samplerate, ymin = lower, ymax=upper,), alpha = 0.3) +
+  geom_line(data=h.pred, aes(x=samplerate, y=pred.fit)) +
+  facet_wrap(~species) + 
+  my.theme +
+  theme(legend.position="none") +
+  xlab("Sample rate") +
+  ylab("Precision of detections") 
+h.plot
+
+#Sample rate*compression on p(recall)
+
+#8b. Compression and sample rate effect on recall----
+r.pred <- data.frame(pred = predict(r1.oven, newdat, se=TRUE, type="response")) %>% 
+  cbind(newdat) %>% 
+  mutate(species="OVEN") %>% 
+  rbind(data.frame(pred = predict(r1.coni, newdat, se=TRUE)) %>% 
+          cbind(newdat) %>% 
+          mutate(species="CONI")) %>% 
+  dplyr::select(pred.fit, pred.se.fit, samplerate.s, compressiontype, species) %>% 
+  unique() %>% 
+  mutate(samplerate = samplerate.s*22050 + 22050,
+         upper = pred.fit + 1.96*pred.se.fit,
+         lower = pred.fit - 1.96*pred.se.fit)
+
+r.pred$species <- factor(r.pred$species, labels=c("Common Nighthawk", "Ovenbird"))
+
+r.plot <- ggplot() +
+  geom_ribbon(data=r.pred, aes(x=samplerate, ymin = lower, ymax=upper, group=compressiontype), alpha = 0.3) +
+  geom_line(data=r.pred, aes(x=samplerate, y=pred.fit, colour=compressiontype)) +
+  scale_colour_viridis_d(name="Compression treatment\n (filetype-compression rate)") +
+  facet_wrap(~species) + 
+  my.theme +
+  xlab("Sample rate") +
+  ylab("Recall of recordings")  +
+  theme(legend.position="bottom")
+r.plot
+
+#8c. Put together----
+rh.plot <- grid.arrange(h.plot, r.plot,
+                        widths = c(1),
+                        heights = c(4,5),
+                        layout_matrix=rbind(c(1), c(2)))
+
+#8d. Save----
+ggsave(rh.plot, filename="figures/Recognizers.jpeg", width=6, height=8)

7e8e3864df7d57913df37e2138426440912f0eaf
ecknight <ellyknight@gmail.com>
2022-12-15 22:52:18 -0700

    Calculate recall properly for random effects, switch to brms for bayesian analysis

--- script/recognizer_analysis.R
+++ script/recognizer_analysis.R
@@ -1,6 +1,5 @@
 library(tidyverse)
-library(lme4)
-library(MuMIn)
+library(brms)
 library(gridExtra)
 
 #NOTE: 100 recordings with confirmed detections were scanned for each species and for each treatment combination
@@ -23,7 +22,7 @@ hit <- rbind(oven, coni) %>%
          samplerate.s = (samplerate-22050)/22050) %>% 
   dplyr::filter(!is.na(detection))
 
-#3. Summarize at recording level----
+#3. Calculate precision----
 rec <- hit %>% 
   group_by(recording, species, samplerate, samplerate.s, compressiontype) %>% 
   summarize(n=n(),
@@ -31,80 +30,101 @@ rec <- hit %>%
   ungroup() %>% 
   mutate(precision = hits/n)
 
-#4. Summarize at species level----
-spp <- rec %>% 
-  group_by(species, samplerate.s, samplerate, compressiontype) %>% 
-  summarize(precision = mean(precision),
-            hits = n()) %>% 
-  ungroup() %>% 
-  mutate(files=case_when(compressiontype=="mp3_96" & species=="CONI" & samplerate==44100 ~ 93,
-                          compressiontype=="mp3_96" & species=="OVEN" & samplerate==44100 ~ 64,
-                          !is.na(compressiontype) ~ 100),
-         recall=hits/files)
-spp
+#4. Get list of all files----
+#Get list of files processed for combo that doesn't have 1000
+# files <- data.frame(file = list.files("/Volumes/ECK004/AudioFiles/Compression/Recognizer Files/CONI/Treatments", recursive=TRUE, pattern="*.wav"),
+#                          species="CONI") %>% 
+#   rbind(data.frame(file=list.files("/Volumes/ECK004/AudioFiles/Compression/Recognizer Files/OVEN/Treatments", recursive = TRUE, pattern="*.wav"),
+#                    species="OVEN")) %>% 
+#   mutate(recording = str_sub(file, -36, -1)) %>% 
+#   separate(file, into=c("treatment", "file"), sep="/") %>% 
+#   separate(treatment, into=c("samplerate", "compressionrate", "filetype")) %>% 
+#   dplyr::filter(!samplerate %in% c("results", "results1")) %>% 
+#   mutate(compressiontype = ifelse(compressionrate %in% c("320", "96"), paste0("mp3_", compressionrate), "wav"),
+#          samplerate = as.numeric(samplerate)) %>% 
+#   dplyr::select(recording, species, samplerate, compressiontype)
+# 
+# write.csv(files, "data/recognizerfilelist.csv", row.names = FALSE)
 
-#5. Make a dataframe for recall analysis----
-recall <- data.frame()
-for(i in 1:nrow(spp)){
-  recall.i <- data.frame(species = rep(spp$species[i],spp$files[i]),
-                        samplerate.s = rep(spp$samplerate.s[i],spp$files[i]),
-                        compressiontype = rep(spp$compressiontype[i]), spp$files[i],
-                        hit = c(rep(1, spp$hits[i]), rep(0, spp$files[i]-spp$hits[i])))
-  recall <- rbind(recall.i, recall)
-}
+files <- read.csv("data/recognizerfilelist.csv")
+
+#5. Add recall of each file----
+dat <- files %>% 
+  left_join(rec) %>% 
+  mutate(recall = ifelse(is.na(precision), 0, 1),
+         samplerate.s = (samplerate-22050)/22050)
 
 #6. Visualize----
 
 #6a. Precision----
-ggplot(rec) + 
-  geom_smooth(aes(x=samplerate.s, y=precision, colour=species)) +
+ggplot(dat) + 
+  geom_smooth(aes(x=samplerate, y=precision, colour=species)) +
   facet_grid(species~compressiontype, scales="free")
 
 #6b. Recall----
-ggplot(recall) +
-  geom_smooth(aes(x=samplerate.s, y=hit, colour=species)) +
+ggplot(dat) +
+  geom_smooth(aes(x=samplerate, y=recall, colour=species)) +
   facet_grid(species~ compressiontype)
 
 #7. Model----
+dat.coni <- dplyr::filter(dat, species=="CONI")
+dat.oven <- dplyr::filter(dat, species=="OVEN")
 
 #7a. Probability of detection being true----
 #CONI
-rec.coni <- dplyr::filter(rec, species=="CONI")
-h0.coni <- lm(precision ~ samplerate.s*compressiontype,
-            data=rec.coni, na.action = "na.fail")
-dredge(h0.coni)
-
-h1.coni <- lm(precision ~ 1, data=rec.coni)
-summary(h1.coni)
+p.coni.b <- brm(precision ~ samplerate.s*compressiontype + (1|recording),
+                data = dat.coni, 
+                warmup = 1000, 
+                iter   = 5000, 
+                chains = 3, 
+                inits  = "random",
+                cores  = 6)
+summary(p.coni.b)
 #no effect
 
 #OVEN
-rec.oven <- dplyr::filter(rec, species=="OVEN")
-h0.oven <- lm(precision ~ samplerate.s*compressiontype,
-                 data=rec.oven, na.action = "na.fail")
-dredge(h0.oven)
+p.oven.b <- brm(precision ~ samplerate.s*compressiontype + (1|recording),
+                data = dat.oven, 
+                warmup = 1000, 
+                iter   = 5000, 
+                chains = 3, 
+                inits  = "random",
+                cores  = 6)
+summary(p.oven.b)
+#effect of samplerate, so rerun
 
-h1.oven <- lm(precision ~ samplerate.s,
-              data=rec.oven, na.action = "na.fail")
-summary(h1.oven)
-#Strong effect of sample rate
+p.oven.b <- brm(precision ~ samplerate.s + (1|recording),
+                data = dat.oven, 
+                warmup = 1000, 
+                iter   = 5000, 
+                chains = 3, 
+                inits  = "random",
+                cores  = 6)
+summary(p.oven.b)
 
 #7b. Recording level recall----
 #CONI
-recall.coni <- dplyr::filter(recall, species=="CONI")
-r0.coni <- glm(hit ~ samplerate.s*compressiontype, data=recall.coni, na.action="na.fail")
-dredge(r0.coni)
-
-r1.coni <- glm(hit ~ 1, data=recall.coni, na.action="na.fail")
-summary(r1.coni)
+r.coni.b <- brm(recall ~ samplerate.s*compressiontype + (1|recording),
+                family="bernoulli",
+                data = dat.coni, 
+                warmup = 1000, 
+                iter   = 5000, 
+                chains = 3, 
+                inits  = "random",
+                cores  = 6)
+summary(r.coni.b)
+#marginal effect of 96 compression rate
 
 #OVEN
-recall.oven <- dplyr::filter(recall, species=="OVEN")
-r0.oven <- glm(hit ~ samplerate.s*compressiontype, data=recall.oven, na.action="na.fail")
-dredge(r0.oven)
-
-r1.oven <- glm(hit ~ 1, data=recall.oven, na.action="na.fail")
-summary(r1.oven)
+r.oven.b <- brm(recall ~ samplerate.s*compressiontype + (1|recording),
+                family="bernoulli",
+                data = dat.oven, 
+                warmup = 1000, 
+                iter   = 5000, 
+                chains = 3, 
+                inits  = "random",
+                cores  = 6)
+summary(r.oven.b)
 
 #8. Plot----
 my.theme <- theme_classic() +
@@ -118,36 +138,6 @@ my.theme <- theme_classic() +
         legend.text=element_text(size=12),
         legend.title=element_text(size=12),
         plot.title=element_text(size=12, hjust = 0.5))
-
-newdat <- expand.grid(compressiontype=unique(hit$compressiontype),
-                      samplerate.s = seq(0, 1, 0.01))
-
-h.pred <- data.frame(pred = predict(h1.oven, newdat, se=TRUE)) %>% 
-  cbind(newdat) %>% 
-  mutate(species="OVEN") %>% 
-  rbind(data.frame(pred = predict(h1.coni, newdat, se=TRUE)) %>% 
-          cbind(newdat) %>% 
-          mutate(species="CONI")) %>% 
-  dplyr::select(pred.fit, pred.se.fit, samplerate.s, species) %>% 
-  unique() %>% 
-  mutate(samplerate = samplerate.s*22050 + 22050,
-         upper = pred.fit + 1.96*pred.se.fit,
-         lower = pred.fit - 1.96*pred.se.fit)
-
-h.pred$species <- factor(h.pred$species, labels=c("Common Nighthawk", "Ovenbird"))
-rec$species <- factor(rec$species, labels=c("Common Nighthawk", "Ovenbird"))
-
-h.plot <- ggplot() +
-  geom_point(data=rec, aes(x=samplerate, y=precision), pch=21, colour="grey50", fill="grey70", size=2) +
-  geom_ribbon(data=h.pred, aes(x=samplerate, ymin = lower, ymax=upper,), alpha = 0.3) +
-  geom_line(data=h.pred, aes(x=samplerate, y=pred.fit)) +
-  scale_x_continuous(limits = c(20000, 45000), breaks = c(20000, 30000, 40000), labels=c("20", "30", "40")) +
-  facet_wrap(~species) + 
-  my.theme +
-  theme(legend.position="none") +
-  xlab("Sample rate (kHz)") +
-  ylab("Precision of detections") 
-h.plot
 
 #8d. Save----
 ggsave(h.plot, filename="figures/Recognizers.jpeg", width=6, height=4)

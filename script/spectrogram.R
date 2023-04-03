2e621e0ba437bc365c3560aec563af09a9290a20
ecknight <ellyknight@gmail.com>
2023-04-03 12:50:16 -0600

    Change spectrogram figure

--- script/spectrogram.R
+++ script/spectrogram.R
@@ -1,51 +1,76 @@
 library(tidyverse)
 library(tuneR)
-library(seewave)
-library(gridExtra)
+library(soundgen)
 
-my.theme <- theme_classic() +
-  theme(text=element_text(size=12, family="Arial"),
-        axis.text.x=element_text(size=12),
-        axis.text.y=element_text(size=12),
-        axis.title.x=element_text(margin=margin(10,0,0,0)),
-        axis.title.y=element_text(margin=margin(0,10,0,0)),
-        axis.line.x=element_line(linetype=1),
-        axis.line.y=element_line(linetype=1),
-        legend.text=element_text(size=12),
-        legend.title=element_text(size=12),
-        plot.title=element_text(size=12, hjust = 0.5))
+files <- list.files("figures/wav", full.names = TRUE, pattern="*.wav")
 
-sr <- c("wav", "mp3_320", "mp3_96", "", "", "", "", "", "")
-cmp <- c("", "", "44100 Hz", "", "", "32000 Hz", "", "", "22050 Hz")
-
-files <- list.files("figures/wav", full.names = TRUE)
-
-i <- 1
-specs <- list()
 for(i in 1:length(files)){
   
   wav.i <- readWave(files[i], from=53, to=56, units="seconds")
   
-  specs[[i]] <- ggspectro(wav.i) +
-    stat_contour(geom="polygon", aes(fill=..level..), bins=30, show.legend=FALSE) +
-    scale_fill_continuous(name="Amplitude\n(dB)\n", limits=c(-40,0),
-                          na.value="transparent", low="white", high="black") + 
-    xlab(sr[i]) + 
-    ylab(cmp[i]) +
-    my.theme +
-    theme(axis.title.y.right = element_text(margin = unit(c(0, 0, 0, 20), 'mm')),
-          axis.title.x.top = element_text(margin = unit(c(0, 0, 0, 20), 'mm')))
+  if(i %in% c(1,4)){
+    spectrogram(wav.i, cores=6,
+                colorTheme='bw',
+                ylab="",
+                xlab="",
+                contrast=0.1,
+                osc = "none",
+                width = 6,
+                height = 4,
+                res = 300,
+                units = "in",
+                xaxt = "n",
+                bg = NA,
+                savePlots = paste0("figures/wav/", i))
+  }
+  if(i %in% c(2,3,5,6)){
+    spectrogram(wav.i, cores=6,
+                colorTheme='bw',
+                ylab="",
+                xlab="",
+                contrast=0.1,
+                osc = "none",
+                width = 6,
+                height = 4,
+                res = 300,
+                units = "in",
+                xaxt = "n",
+                yaxt = "n",
+                bg = NA,
+                savePlots = paste0("figures/wav/", i))
+  }
+  if(i==7){
+    spectrogram(wav.i, cores=6,
+                colorTheme='bw',
+                ylab="",
+                xlab="",
+                contrast=0.1,
+                osc = "none",
+                width = 6,
+                height = 4,
+                res = 300,
+                units = "in",
+                bg = NA,
+                savePlots = paste0("figures/wav/", i))
+
+  }
+  if(i %in% c(8,9)){
+    spectrogram(wav.i, cores=6,
+                colorTheme='bw',
+                ylab="",
+                xlab="",
+                contrast=0.1,
+                osc = "none",
+                width = 6,
+                height = 4,
+                res = 300,
+                units = "in",
+                yaxt = "n",
+                bg = 'transparent',
+                savePlots = paste0("figures/wav/", i))
+  }
+
   
 }
 
-ggsave(grid.arrange(specs[[1]], specs[[2]], specs[[3]],
-                    specs[[4]], specs[[5]], specs[[6]],
-                    specs[[7]], specs[[8]], specs[[9]],
-                    heights=c(2,1.6,1.2),
-                    widths=c(2,2,2),
-                    layout_matrix=(rbind(c(1,2,3), c(4,5,6), c(7,8,9))),
-                    left = "Frequency (kHz)",
-                    bottom = "Time (s)",
-                    right = "Sample rate (Hz) treatment",
-                    top = "Compression type (file type_bitrate)"),
-       filename="figures/spectrogram.jpeg", width=12, height = 6)
+
